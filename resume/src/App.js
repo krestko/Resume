36faba0cefaa868af9f18d42eff1a8286689710b
krestko <Katherine.V.Restko@gmail.com>
2019-01-08 13:12:16 -0600

    first draft of website homepage

---
+++ resume/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage'; 
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Route exact path='/' component={HomePage} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+export default App;
---
+++ resume/src/pages/HomePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import '../App.css';
+import './HomePage.css';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="Grid-Container">
+        <div className="Grid-Item-1 Content">K</div>
+        <div className="Grid-Item-2 Content">A</div>
+        <div className="Grid-Item-3 Content">T</div>
+        <div className="Grid-Item-4 Content">H</div>
+        <div className="Grid-Item-5 Content">E</div>
+        <div className="Grid-Item-6 Content">R</div>
+        <div className="Grid-Item-7 Content">I</div>
+        <div className="Grid-Item-8 Content">N</div>
+        <div className="Grid-Item-9 Content">E</div>
+        <div className="Grid-Item-10 Content">R</div>
+        <div className="Grid-Item-11 Content">E</div>
+        <div className="Grid-Item-12 Content">S</div>
+        <div className="Grid-Item-13 Content">T</div>
+        <div className="Grid-Item-14 Content">K</div>
+        <div className="Grid-Item-15 Content">O</div>
+      </div>      
+    );
+  }
+}
+export default HomePage;
